ffb7b87b007a675214dd58587f764ff0de09d6b3
srujan-patil <srujanpanthula90@gmail.com>
2026-01-04 03:07:07 +0530

    version2

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ShopEasy - Online Store</title>
+    <title>ShopEasy1 - Online Store</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
